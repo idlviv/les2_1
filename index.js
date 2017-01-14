c5daa2e070e117cafbcac50c2a782b0c227a23ce
idlviv <idlviv@rambler.ru>
2017-01-14 14:42:25 +0200

    2_1 Final

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 //Ваш код будет здесь
+var state = {
+  items:[]
+};
+var d = localStorage.getItem('tasks');
+
+if (d){
+  state = JSON.parse(d);
+}
+
+window.addEventListener('load', function wLoader() {
+  'use strict';
+  var button = document.querySelector('button');
+  var input = document.querySelector('input');
+  var list = document.querySelector('ul');
+  var i;
+  var liList = document.querySelectorAll('li');
+
+  function update() {
+  list.innerHTML = '';
+    state.items.sort();
+  for (i = 0; i < state.items.length; i++){
+    var newLi = document.createElement('li');
+    newLi.innerHTML = state.items[i];
+    list.appendChild(newLi);
+  }
+  }
+  update();
+  button.addEventListener('click', btnClick);
+  document.addEventListener('keydown', btnClick);
+  function btnClick (event) {
+    if (event.keyCode === 13  || event.target === button) {
+      if (!input.value) {
+        return;
+      }
+
+      state.items.push(input.value);
+      localStorage.setItem('tasks', JSON.stringify(state));
+      input.value = '';
+      update();
+    }
+  }
+});
